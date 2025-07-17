cef93668813736f08bfecf6d158b2de878ce3b8c
ayushrajsingh0542 <aayushrajsingh11@gmail.com>
2025-07-18 00:40:37 +0530

    made base url dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL="/api"//because of aws and nginx earlier was http://localhost:3030
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3030" : "/api";
+//because of aws and nginx earlier was http://localhost:3030
